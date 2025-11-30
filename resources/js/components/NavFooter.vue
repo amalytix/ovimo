64260f2073c144e44ed655277b41bc63852c4255
Trutz Fries <trutz@trutz.de>
2025-11-30 13:47:46 +0100

    Fix link

--- resources/js/components/NavFooter.vue
+++ resources/js/components/NavFooter.vue
@@ -8,6 +8,7 @@ import {
 } from '@/components/ui/sidebar';
 import { toUrl } from '@/lib/utils';
 import { type NavItem } from '@/types';
+import { Link } from '@inertiajs/vue3';
 
 interface Props {
     items: NavItem[];
@@ -29,6 +30,7 @@ defineProps<Props>();
                         as-child
                     >
                         <a
+                            v-if="item.external"
                             :href="toUrl(item.href)"
                             target="_blank"
                             rel="noopener noreferrer"
@@ -36,6 +38,10 @@ defineProps<Props>();
                             <component :is="item.icon" />
                             <span>{{ item.title }}</span>
                         </a>
+                        <Link v-else :href="item.href">
+                            <component :is="item.icon" />
+                            <span>{{ item.title }}</span>
+                        </Link>
                     </SidebarMenuButton>
                 </SidebarMenuItem>
             </SidebarMenu>
